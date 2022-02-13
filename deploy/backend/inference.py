8ed5433b524e3897159f88b2aacfd9672746f255
gtarpenning <gtarpenning@gmail.com>
2022-02-12 16:15:55 -0800

    structure reorganization for skforkast model checkpoints, need better solution ...

--- deploy/backend/inference.py
+++ deploy/backend/inference.py
@@ -8,9 +8,9 @@ def predict():
 	logging.info("Logging prediction hit")
 
 	# Load model
-	forecaster_ff = load("saved_models/skforecast1hr/fossil_fuel_forecaster1hr.py")
-	forecaster_renewable = load("saved_models/skforecast1hr/renewable_forecaster1hr.py")
-	forecaster_other = load("saved_models/skforecast1hr/other_forecaster1hr.py")
+	forecaster_ff = load("./skforecast1hr/fossil_fuel_forecaster1hr.py")
+	forecaster_renewable = load("./skforecast1hr/renewable_forecaster1hr.py")
+	forecaster_other = load("./skforecast1hr/other_forecaster1hr.py")
 
 	# Load train data
 	train_x_fn = "data/X_train_california_2020-2021.csv"
