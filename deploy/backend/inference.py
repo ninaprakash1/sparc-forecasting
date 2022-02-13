76863920e5d7b0291a03c45428320d682349a7b5
gtarpenning <gtarpenning@gmail.com>
2022-02-12 16:18:23 -0800

    added more logging and again included required files in docker image, gross duplicate files thoughgit add . !

--- deploy/backend/inference.py
+++ deploy/backend/inference.py
@@ -6,17 +6,23 @@ from joblib import load
 
 def predict():
 	logging.info("Logging prediction hit")
+	print("Logging prediction hit -- print statement")
 
 	# Load model
 	forecaster_ff = load("./skforecast1hr/fossil_fuel_forecaster1hr.py")
 	forecaster_renewable = load("./skforecast1hr/renewable_forecaster1hr.py")
 	forecaster_other = load("./skforecast1hr/other_forecaster1hr.py")
 
+	print("Loaded models")	
+
 	# Load train data
-	train_x_fn = "data/X_train_california_2020-2021.csv"
+	train_x_fn = "X_train_california_2020-2021.csv"
 	weather_vars = ["tempC","uvIndex","WindGustKmph","cloudcover","humidity","precipMM"]
 	data = pd.read_csv(train_x_fn)
 	
+
+	print("loaded training data")
+
 	pred_ff = forecaster_ff.predict(steps=24, exog = data[weather_vars])
 	pred_renewable = forecaster_renewable.predict(steps=24, exog = data[weather_vars])
 	pred_other = forecaster_other.predict(steps=24, exog = data[weather_vars])
