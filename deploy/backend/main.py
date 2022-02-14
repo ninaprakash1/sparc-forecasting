8d7ee0d662befe914afe62e2f44bf78839093172
gtarpenning <gtarpenning@gmail.com>
2022-02-13 18:58:30 -0800

    Added comments for slightly altered server structure

--- deploy/backend/main.py
+++ deploy/backend/main.py
@@ -11,25 +11,33 @@ from inference import predict
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome from the API"}
+""" Import Models """
 
 
-@app.get("/echo/{message}")
-def read_root(message: str):
-    return {"message": f"{message}"}
+""" Load Data """
 
+
+""" Class Definition Here? (store models in state)"""
 @app.get("/predict")
 def get_prediction():
     return {"result": predict()}
 
 
-@app.post("/{get_info}")
-def get_image(get_info: str):
-    return {"name": get_info}
+# """ Sample Post Request """
+# @app.post("/{get_info}")
+# def get_image(get_info: str):
+#     return {"name": get_info}
 
+@app.get("/")
+def read_root():
+    """ Root endpoint, TODO: display basic info (readme?) """
+    return {"message": "Welcome from the API"}
+
+
+""" Example query param request """
+# @app.get("/help/{message}")
+# def read_root(message: str):
+#     return {"message": f"{message}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
