431011ff233a510c08c071a35adc5644df00908a
gtarpenning <gtarpenning@gmail.com>
2022-02-15 20:10:02 -0800

    more small organization changes

--- deploy/backend/inference.py
+++ deploy/backend/inference.py
@@ -8,12 +8,12 @@ def predict():
 	logging.info("Logging prediction hit")
 
 	# Load model
-	forecaster_ff = load("saved_models/skforecast1hr/fossil_fuel_forecaster1hr.py")
-	forecaster_renewable = load("saved_models/skforecast1hr/renewable_forecaster1hr.py")
-	forecaster_other = load("saved_models/skforecast1hr/other_forecaster1hr.py")
+	forecaster_ff = load("./skforecast1hr/fossil_fuel_forecaster1hr.py")
+	forecaster_renewable = load("./skforecast1hr/renewable_forecaster1hr.py")
+	forecaster_other = load("./skforecast1hr/other_forecaster1hr.py")
 
 	# Load train data
-	train_x_fn = "data/X_train_california_2020-2021.csv"
+	train_x_fn = "./X_train_california_2020-2021.csv"
 	weather_vars = ["tempC","uvIndex","WindGustKmph","cloudcover","humidity","precipMM"]
 	data = pd.read_csv(train_x_fn)
 	
