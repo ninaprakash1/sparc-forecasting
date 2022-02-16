7b5c57dbe838eba67c784858becaaf4337fb91ba
gtarpenning <gtarpenning@gmail.com>
2022-02-15 21:09:37 -0800

    Backend endpoint changes for new retrain feature

--- deploy/backend/main.py
+++ deploy/backend/main.py
@@ -5,8 +5,11 @@ from fastapi import File
 from fastapi import FastAPI
 from fastapi import UploadFile
 import numpy as np
+import logging
 
 from inference import predict
+from train import train_renewable_model, train_fossil_fuel_model, train_other_model
+from utils import test_model
 
 
 app = FastAPI()
@@ -23,11 +26,6 @@ def get_prediction():
     return {"result": predict()}
 
 
-# """ Sample Post Request """
-# @app.post("/{get_info}")
-# def get_image(get_info: str):
-#     return {"name": get_info}
-
 @app.get("/")
 def read_root():
     """ Root endpoint, TODO: display basic info (readme?) """
@@ -35,9 +33,28 @@ def read_root():
 
 
 """ Example query param request """
-# @app.get("/help/{message}")
-# def read_root(message: str):
-#     return {"message": f"{message}"}
+@app.get("/train/{param_str}")
+def train_models(param_str: str):
+    """ Experimental endpoint to retrain models, accepts param str """
+    logging.info(f"Recieved request for retrain with param_str: {param_str}")
+    if "years=2" in param_str:
+        logging.info("retraining with n=730")
+
+    try: 
+        train_renewable_model(730)
+        logging.info(f"Renewable test: {test_model('renewable_forecaster1hr.py')}")
+
+        train_fossil_fuel_model(730)
+        logging.info(f"Fossil test: {test_model('fossil_fuel_forecaster1hr.py')}")
+
+        train_other_model(730)
+        logging.info(f"Other test: {test_model('other_forecaster1hr.py')}")
+        
+        return {"result": f"Retrain Successful!"}
+    except Exception as e:
+        return {"result": f"{e}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
+
+    
