85653d61517f1bcddb59aa060f0f873d344902fb
kumono <kumono@DN0a1e47e8.SUNet>
2022-02-28 22:45:58 -0800

    update train and inferency

--- deploy/backend/inference.py
+++ deploy/backend/inference.py
@@ -10,12 +10,13 @@ def predict(num_days=30):
 
 	# Load models - check train.py for aggregated columns
 	forecaster_battery = load("./skforecast1hr/Batteries_forecaster1hr.py")
-	forecaster_ff = load("./skforecast1hr/fossil_fuel_forecaster1hr.py")
-	forecaster_Hydro = load("./skforecast1hr/Hydro_forecaster1hr.py")
-	forecaster_other = load("./skforecast1hr/other_forecaster1hr.py")
-	forecaster_renewable = load("./skforecast1hr/renewable_forecaster1hr.py")
-	forecaster_renewable_o = load("./skforecast1hr/renewable_other_forecaster1hr.py")
+	forecaster_ff = load("./skforecast1hr/Fossil_fuel_forecaster1hr.py")
+	forecaster_hydro = load("./skforecast1hr/Hydro_forecaster1hr.py")
+	forecaster_other = load("./skforecast1hr/Other_forecaster1hr.py")
+	forecaster_renewable = load("./skforecast1hr/Renewable_forecaster1hr.py")
+	forecaster_renewable_o = load("./skforecast1hr/Renewable_other_forecaster1hr.py")
 	forecaster_solar = load("./skforecast1hr/Solar_forecaster1hr.py")
+	forecaster_total = load("./skforecast1hr/Total_forecaster1hr.py")
 
 	# Load train data
 	weather_vars = ["tempC","uvIndex","WindGustKmph","cloudcover","humidity","precipMM"]
@@ -24,14 +25,14 @@ def predict(num_days=30):
 
 	pred_battery = forecaster_battery.predict(steps = 24)
 	pred_ff = forecaster_ff.predict(steps=24)
-	pred_hydro = forecaster_Hydro.predict(steps = 24)
+	pred_hydro = forecaster_hydro.predict(steps = 24)
 	pred_other = forecaster_other.predict(steps=24)
 	pred_renewable = forecaster_renewable.predict(steps=24)
 	pred_renewable_o = forecaster_renewable_o.predict(steps = 24)
 	pred_solar = forecaster_solar.predict(steps = 24)
+	pred_total = forecaster_total.predict(steps = 24)
 
-	pred_wind = pred_renewable - pred_renewable_o - pred_solar
+	pred_wind = pred_renewable - pred_renewable_o - pred_solar - pred_hydro
 
 	return {'battery':pred_battery, 'fossil_fuel': pred_ff, 'hydro': pred_hydro, 'other': pred_other,
-			'renewable_other': pred_renewable_o, 'solar': pred_solar, 'wind': pred_wind
-			}
+			'renewable_other': pred_renewable_o, 'solar': pred_solar, 'wind': pred_wind, 'total': pred_total}
