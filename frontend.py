1edc05a846b0c4eac73b342a5ed7d1e4703d1c02
Nina Prakash <ninap514@gmail.com>
2022-03-02 10:59:09 -0800

    Add option for manual energy cons entry

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import json
 import requests
 import streamlit as st
 from utils import generate_graph_historical_and_forecasted
-from deploy.backend.utils import compute_co2, ACTIVITY_USAGE_KWH
+from deploy.backend.utils import compute_co2
 
 ###
 # Main app components
@@ -29,12 +29,21 @@ st.sidebar.markdown("*Kun Guo, Nina Prakash, Griffin Schillinger Tarpenning | St
 
 # User Input
 
-activity = st.selectbox("Select an activity", ["Charge an EV (Level 1)", "Charge an EV (Level 2)",
-                                               "Charge an EV (Level 3)", "Run the washing machine",
-                                               "Run the dryer", "Take a hot shower",
-                                               "Run central AC", "Run a space heater",
-                                               "Run hot water heater", "Run the dishwasher",
-                                               "Watch TV"])
+ACTIVITY_USAGE_KWH = { # these are all kW x 1
+    "Charge an EV (Level 1)": 1, # https://rmi.org/electric-vehicle-charging-for-dummies/#:~:text=If%20you%20just%20plug%20an,7%20kW%20and%2019%20kW.
+    "Charge an EV (Level 2)": 13,
+    "Charge an EV (Level 3)": 50,
+    "Run the washing machine": 5, # https://blog.arcadia.com/electricity-costs-10-key-household-products/#:~:text=An%20average%20cycle%20for%20a,to%20run%20for%2030%20minutes.
+    "Run the dryer": 4, # https://majorenergy.com/how-much-electricity-does-a-dryer-use/#:~:text=Dryers%20are%20typically%20somewhere%20in,and%206%20kilowatts%20an%20hour.
+    "Take a hot shower": 8.2, # https://greengeekblog.com/tools/shower-cost-calculator/#:~:text=An%20average%208.2%20minute%20shower,energy%20to%20heat%20our%20water.,
+    "Run central AC": 3.5, # https://energyusecalculator.com/electricity_centralac.htm
+    "Run a space heater": 1.5, # https://experthomereport.com/do-space-heaters-use-a-lot-of-electricity/#:~:text=Although%20amounts%20vary%20per%20space,but%20in%20hours%20of%20kilowatt.
+    "Run hot water heater": 4, # https://www.directenergy.com/learning-center/how-much-energy-water-heater-use#:~:text=Typically%2C%20a%20hot%20water%20heater,month%2C%20or%20%24438%20per%20year.
+    "Run the dishwasher": 1.8, # https://www.inspirecleanenergy.com/blog/sustainable-living/cost-to-run-dishwasher#:~:text=Most%20dishwashers%20use%20an%20average,hours%20(kWh)%20of%20electricity.
+    "Watch TV": 0.1 # https://letsavelectricity.com/how-much-power-does-a-tv-use-in-an-hour/
+}
+
+activity = st.selectbox("Select an activity", ACTIVITY_USAGE_KWH.keys())
 
 activity_present_tense = {
     "Charge an EV (Level 1)": "charging an EV (Level 1)",
@@ -60,11 +69,13 @@ choice = st.radio("Pick one",["Use default energy consumption of {}: {} kWh".for
                                                                              ACTIVITY_USAGE_KWH[activity]), "Enter my own"])
 
 if (choice == "Enter my own"):
-    energy_cons = st.text_input("Energy consumption of {} (kW):".format(activity_present_tense[activity]), ACTIVITY_USAGE_KWH[activity], max_chars=3)
+    energy_cons = st.text_input("Energy consumption of {} (kW):".format(activity_present_tense[activity]), ACTIVITY_USAGE_KWH[activity], max_chars=10)
     try:
-        energy_cons = int(energy_cons)
+        energy_cons = float(energy_cons)
     except:
         st.error("Please enter a number")
+else:
+    energy_cons = ACTIVITY_USAGE_KWH[activity]
 
 clicked_generate = st.button('Generate Forecast')
 
@@ -74,7 +85,7 @@ if (clicked_generate):
     with st.empty():
         st.write(f"Gathering prediction...")
         results, fig2, fig3 = generate_graph_historical_and_forecasted()
-        co2 = compute_co2(results, activity, hour, duration)
+        co2 = compute_co2(results, energy_cons, hour, duration)
         st.success('Model run complete')
 
     # Write output
